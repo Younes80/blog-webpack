fd94bd5e1e2a6c18852377975bb6081f0bd88df6
Younes80 <57212570+Younes80@users.noreply.github.com>
2022-04-27 15:46:25 +0200

    modification update atricle

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.scss';
 const url = 'http://localhost:3000/posts';
 
 const articleContainerElement = document.querySelector('.articles-container');
+const categoriesContainerElement = document.querySelector('.categories');
+
+let articles;
+let filter;
 
 const formatDate = date => {
 	return new Date(date).toLocaleDateString('fr-FR', {
@@ -14,25 +18,33 @@ const formatDate = date => {
 	});
 };
 
-const createArticles = articles => {
-	const articlesDom = articles.map(article => {
-		// console.log(article);
-		const articleDom = document.createElement('div');
-		articleDom.classList.add('article');
-		articleDom.innerHTML = `
+const createArticles = () => {
+	const articlesDom = articles
+		.filter(article => {
+			if (filter) {
+				return article.category === filter;
+			} else {
+				return true;
+			}
+		})
+		.map(article => {
+			// console.log(article);
+			const articleDom = document.createElement('div');
+			articleDom.classList.add('article');
+			articleDom.innerHTML = `
       <img src="${article.img}" alt="profile" />
       <h2>${article.title}</h2>
       <p class="article-author">${article.author} - ${formatDate(
-			article.createdAt
-		)}</p>
+				article.createdAt
+			)}</p>
       <p class="article-content">${article.content}</p>
       <div class="article-actions">
         <button class="btn btn-danger" data-id=${article.id} >Supprimer</button>
         <button class="btn btn-primary" data-id=${article.id} >Modifier</button>
       </div>
     `;
-		return articleDom;
-	});
+			return articleDom;
+		});
 	articleContainerElement.innerHTML = '';
 	articleContainerElement.append(...articlesDom);
 
@@ -63,11 +75,55 @@ const createArticles = articles => {
 	});
 };
 
+const displayMenuCategories = categoriesArr => {
+	const liElements = categoriesArr.map(categoryElem => {
+		const li = document.createElement('li');
+		li.innerHTML = `${categoryElem[0]} ( <strong>${categoryElem[1]}</strong> )`;
+		li.addEventListener('click', () => {
+			if (filter === categoryElem[0]) {
+				filter = null;
+				li.classList.remove('active');
+			} else {
+				filter = categoryElem[0];
+				liElements.forEach(li => {
+					li.classList.remove('active');
+				});
+				li.classList.add('active');
+			}
+			createArticles();
+		});
+		return li;
+	});
+
+	categoriesContainerElement.innerHTML = '';
+	categoriesContainerElement.append(...liElements);
+};
+
+const createMenuCategories = () => {
+	const categories = articles.reduce((acc, article) => {
+		if (acc[article.category]) {
+			acc[article.category]++;
+		} else {
+			acc[article.category] = 1;
+		}
+		return acc;
+	}, {});
+
+	const categoriesArr = Object.keys(categories)
+		.map(category => {
+			return [category, categories[category]];
+		})
+		.sort((c1, c2) => c1[0].localeCompare(c2[0]));
+
+	displayMenuCategories(categoriesArr);
+};
+
 const fetchArticle = async () => {
 	try {
 		const response = await fetch(url);
-		const articles = await response.json();
-		createArticles(articles);
+		articles = await response.json();
+		createArticles();
+		createMenuCategories();
 	} catch (e) {
 		console.log('e : ', e);
 	}
